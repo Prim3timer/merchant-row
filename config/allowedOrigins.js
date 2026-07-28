808b9e70522e48de482b8ef7a61df046a14c4e13
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2026-07-28 16:39:20 -0400

    done a lot

---
+++ config/allowedOrigins.js
@@ -0,0 +1,11 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3500",
+  "http://127.0.0.1:5500",
+  "https://retail-solutions.onrender.com",
+  // "https://prim3timer.github.io/Retailer",
+  "https://prim3timer.github.io",
+];
+
+module.exports = allowedOrigins;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConnect");
 const cors = require("cors");
+const corsOptions = require("./config/corOptions.js");
 const app = express();
 const { people } = require("./usersDB.js");
 
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/", require("./routes/root"));
