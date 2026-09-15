2c2ee56e1093bc42a0ec8368b2262906213bc5c9
Prim3timer <dikeekwelie@gmail.com>
2026-09-15 17:43:58 -0400

    never mind

--- controllers/workoutAuthController.js
+++ controllers/workoutAuthController.js
@@ -4,13 +4,12 @@ const asyncHandler = require("express-async-handler");
 
 const handleLogin = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
-  const foundUser = await WorkUserSchema.findOne({ username }).exec();
-  console.log({ foundUserVer: foundUser.verified });
-  if (foundUser.verified === false) return res.status(401).json({message: "unverified"})
+  console.log({ reqBody: req.body });
   if (!username || !password)
     return res
       .status(400)
       .json({ message: "username and password are required" });
+  const foundUser = await WorkUserSchema.findOne({ username }).exec();
   if (!foundUser) return res.status(401).json({message: 'no such username'});
   const match = await bcrypt.compare(password, foundUser.password);
   if (!match) return res.status(401).json({ message: "unauthorized" });
